42dfbaaf18bd7dbf51a8d3daa420769099367e4a
manuel <manuelnpascual@gmail.com>
2026-02-19 17:42:54 +0100

    home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,42 @@
 import { useTranslations } from 'next-intl';
-
+import Image from 'next/image';
+import homeImage from '../../public/assets/images/home/hero-image-kali.webp';
+import dividerSvg from '../../public/assets/icons/divider.svg';
 export default function HomePage() {
   const t = useTranslations('pages.home');
 
   return (
     <main>
-<h1 className='title text-white text-[40px] lg:text-[80px] font-bold max-w-[1000px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto text-center absolute'>{t('title')}</h1>
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
-<img src="https://www.marie-claire.es/wp-content/uploads/sites/2/2024/07/08/668bc0fc66288.jpeg" alt="Home" />
+      <div className="relative h-screen min-h-[700px] max-h-[870px] overflow-hidden flex items-center justify-center">
+
+        <Image
+          src={homeImage}
+          alt="Home background"
+          fill
+          priority
+          className="object-cover scale-[1.3] origin-top-left object-[-30px_center]"
+        />
+
+        <h1 className="relative z-10 text-[100px] text-white title text-center max-w-[585px]">
+          {t('title')}
+        </h1>
+
+        <Image
+          src={dividerSvg}
+          alt="divider"
+          width={1512}
+          height={193}
+          className="absolute bottom-0 left-0 w-full pointer-events-none"
+        />
+
+      </div>
+
+
+      <div className="flex flex-col items-center justify-center h-[100vh]">
+        <h2 className="text-[40px] text-black title text-center max-w-[585px]"> {t('subtitle')}</h2>
+      </div>
+
     </main>
+
   );
 }
