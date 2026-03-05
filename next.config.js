1d3e2b51f11bd6dd3a8801c84b5bb1643ad6c439
manuel <manuelnpascual@gmail.com>
2026-03-05 16:00:59 +0100

    config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
