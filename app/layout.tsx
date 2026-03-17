d671a23217b539e8c68b6a25f563223805619e12
manuel <manuelnpascual@gmail.com>
2026-03-17 23:01:25 +0100

    GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import SmoothScrollProvider from "@/components/SmoothScrollProvider";
 import { BASE_URL } from "@/lib/metadata";
 
 const GA_MEASUREMENT_ID = "G-8181TXVVRV";
+const GTM_ID = "GTM-NTH5NXPW";
 
 export const metadata: Metadata = {
   title: "Kali Coliving",
@@ -33,6 +34,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
